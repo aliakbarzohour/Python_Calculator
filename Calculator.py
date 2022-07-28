8fb1fef61718e93dd0515b5fd2d0aa321b0a4bd2
aliakbarzohour <aliakbar.zohour@gmail.com>
2022-07-28 13:07:15 +0430

    ...

--- Calculator.py
+++ Calculator.py
@@ -1,6 +1,3 @@
 
-# hello 
-
-for i in range(0,4):
-	input = input(' [ * ] Type For Calculate : ')
-	print(" Your Answer is ==>  ", eval(input))
+input = input(' [ * ] Type For Calculate : ')
+print(" Your Answer is ==>  ", eval(input))
