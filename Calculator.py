002afefa8293d0d0a0fc363817ee711a93da8183
aliakbarzohour <aliakbar.zohour@gmail.com>
2022-07-29 01:38:33 +0430

    clean Code ...

--- Calculator.py
+++ Calculator.py
@@ -1,3 +1,16 @@
+# ___ Python Calculator Project ___
 
-input = input(' [ * ] Type For Calculate : ')
-print(" Your Answer is ==>  ", eval(input))
+# ___ Explain ___
+#
+# Hello friends, welcome to this super mini Python project.
+# Of course, we know that the power of Python is much more than these, 
+# but the point of this project is to summarize it in two lines and at 
+# the same time be completely functional 
+# (exactly like Unix tools).
+# So, let's start with the basics.
+# _______________
+
+input = input(' [ ? ] Type For Calculate : ')
+print(" [ * ] Your Answer is :  ", eval(input))
+
+# __________________________________
